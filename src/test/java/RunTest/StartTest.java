351c91764869a5995f9393ca7bb5dec830ba3170
user <user@MacBook-Pro-danlex.local>
2021-02-10 18:12:39 +0300

    and faker

--- src/test/java/RunTest/StartTest.java
+++ src/test/java/RunTest/StartTest.java
@@ -1,6 +1,8 @@
 package RunTest;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
 import config.Base;
 import org.junit.jupiter.api.Test;
 
@@ -16,25 +18,26 @@ public class StartTest extends Base {
 
     final String url = "https://demoqa.com/automation-practice-form";
 
-    final String firstName = "Alexander";
-    final String lastName = "Gerasimov";
-    final String userEmail = "as.nanao@gmail.com";
-    final String gender = "Male";
-    final String userNumber = "9251029407";
-    final String dayOfBirth = "26";
-    final String monthOfBirth = "December";
-    final String yearOfBirth = "1994";
-    final String subjectTwo = "History";
-    final String picture = "photo.jpg";
-    final String currentAddress = "New Zealand, Shire";
-    final String state = "Rajasthan";
-    final String city = "Jaipur";
+    final String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            gender = "Male",
+            userNumber = faker.phoneNumber().subscriberNumber(10),
+            dayOfBirth = "26",
+            monthOfBirth = "December",
+            yearOfBirth = "1994",
+            subjectTwo = "History",
+            picture = "photo.jpg",
+            currentAddress = faker.address().streetAddress(),
+            state = "Rajasthan",
+            city = "Jaipur";
 
     @Test
     public void testRun() {
         open("https://demoqa.com/automation-practice-form");
 
         $("#firstName").setValue(firstName);
+
         $("#lastName").setValue(lastName);
         $("#userEmail").setValue(userEmail);
         $x("//*[@id='subjectsContainer']/descendant::input").setValue(subjectTwo).pressEnter();
@@ -51,10 +54,10 @@ public class StartTest extends Base {
         $x("//*[@class='react-datepicker__month']/descendant::" + "div[text()='" + dayOfBirth + "' and not(contains(@class,'react-datepicker__day--outside-month'))]").click();
 
 
-        $(byText("Sports")).click();
-        $(byText("Reading")).click();
-        $(byText("Music")).click();
-
+        ElementsCollection elementsHobs = $$(".custom-checkbox");
+        for (SelenideElement element : elementsHobs) {
+            element.click();
+        }
         $("#uploadPicture").uploadFile(new File("src/test/resources/" + picture));
         $("#currentAddress").setValue(currentAddress);
         $("#state").click();
--- src/test/java/config/Base.java
+++ src/test/java/config/Base.java
@@ -2,23 +2,19 @@ package config;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class Base {
+    public Faker faker = new Faker();
+
 
     @BeforeAll
     public static void setUp() {
+        Configuration.startMaximized = true;
 
     }
-
-    @AfterEach
-    public void clean() {
-        Selenide.clearBrowserCookies();
-        Selenide.clearBrowserLocalStorage();
-    }
-
-
 }
 
 
